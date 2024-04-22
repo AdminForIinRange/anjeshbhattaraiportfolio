f3db99494ba1ccdf5efffba1b1f6d12456c489a8
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-22 18:32:14 +0930

    basic svg import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,10 +8,30 @@ import {
   useMediaQuery,
 } from "@chakra-ui/react";
 
+import Asterisks from "../svg/Asterisks";
+import Image from "next/image";
+ 
+
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <HStack   w={"100%"} h={"100%"} align={"center"} justify={"center"} >
+
+      <VStack w={"100%"} h={"100%"} align={"center"} justify={"center"}>
+
+        <Text fontSize={["200px"]} color={"white"}>
+          Anjesh
+        </Text>
+        <Text fontSize={["60"]} color={"white"} >
+        Full Stack Developer 
+        </Text>
+        <Image  w={100}  h={100}  src={Asterisks}/>
+      </VStack>
+    </HStack>
+    
+    </>
   )
 }
+
 
 export default page
